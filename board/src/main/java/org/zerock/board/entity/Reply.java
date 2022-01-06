b770ff20db10acc6c8dcb267f65af608cb41e3f6
DESKTOP-FP38J33\minHong <coals0329@cocen.com>
2022-01-06 17:58:22 +0900

    [minHong] springBoot day+14 (댓글 json, RestController 처리)

--- board/src/main/java/org/zerock/board/entity/Reply.java
+++ board/src/main/java/org/zerock/board/entity/Reply.java
@@ -21,6 +21,9 @@ public class Reply extends BaseEntity{
     private String replyer;
 
     // https://jamong-icetea.tistory.com/236
+    // org.hibernate.TransientPropertyValueException
+    // https://postitforhooney.tistory.com/entry/JavaJPAHibernate-CascadeType%EB%9E%80-%EA%B7%B8%EB%A6%AC%EA%B3%A0-%EC%A2%85%EB%A5%98
+    // 여기 더 알아보기 , cascade = CascadeType.ALL 설정했을 때 업데이트는 되는데 인설트가 안됨
     @ManyToOne(fetch = FetchType.LAZY)
     private Board board;
 
